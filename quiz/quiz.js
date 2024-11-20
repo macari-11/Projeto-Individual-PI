838a8ecf2d5f7d4a38984547c356b67a25551d80
macari-11 <macari.pinho@sptech.school>
2024-11-20 08:17:09 -0300

    Quiz quase estruturado

---
+++ quiz/quiz.js
@@ -0,0 +1,116 @@
+// CHAMANDO AS VARIAVEIS ATRAVES DO CSS 
+const startGamebutton = document.querySelector(".start-quiz")
+const questionsContainer = document.querySelector(".questions-container")
+const answerContainer = document.querySelector(".answer-container")
+const questionText = document.querySelector(".questions")
+const nextQuestionButton = document.querySelector(".next-question")
+
+var currentQuestionIndex = 0 
+
+nextQuestionButton.addEventListener("click", displayNextQuestion)
+
+// AQUI AO CLICAR NO BOTÃO DE INICIAR  ADICIONAMOS E REMOVEMOS A CLASS HIDE
+// ASSIM ELE FICA DINAMICO
+function startGame(){
+    startGamebutton.classList.add("hide")
+    questionsContainer.classList.remove("hide")
+    
+    displayNextQuestion()
+}
+
+
+
+// NESSA FUNÇÃO 
+function displayNextQuestion(){
+
+    resetState()
+
+    questionText.textContent = questions[currentQuestionIndex].question
+    questions[currentQuestionIndex].answers.forEach(answer => {
+
+        const newAnswer = document.createElement("button")
+        newAnswer.classList.add("answer", "button")
+        newAnswer.textContent = answer.text
+
+
+        // ADICIONANDO INFORMAÇÃO PARA O BOTÃO CRIADO ACIMA
+        if(answer.correct){                               
+            newAnswer.dataset.correct = answer.correct
+        }
+        answerContainer.appendChild(newAnswer)
+
+    // AQUI ELE CAPTA O EVENTO DE CLICK, E APÓS ISSO IRÁ RODAR A FUNÇÃO PARA VERIFICAR 
+    // SE ESTÁ CERTA OU NÃO 
+        newAnswer.addEventListener("click", selectAnswer)
+    })
+}
+
+// NESSA FUNÇÃO FAZEMOS COM QUE O BOTÃO DA PROXÍMA PERGUNRA SUMA RETIRANDO A CLASS DELE
+// E TAMBÉM LIMPAMOS AS RESPOSTAS ANTERIORES RETIRANDO OS BOTÕES DA DIV *ANSWER CONTAINER*
+function resetState(){
+    nextQuestionButton.classList.add("hide")
+        
+    while(answerContainer.firstChild){
+        answerContainer.removeChild(answerContainer.firstChild)
+    }
+}
+
+//VERIFICANDO SE A QUESTÃO ESTÁ CORRETA
+// O EVENT ELE RETORNAR O BOTÃO DO CLICK
+// SE VIER O TRUE NO ELEMENTO SABEMOS QUE FOI A CERTA
+function selectAnswer(event){
+    const answerClicked = event.target
+    
+    nextQuestionButton.classList.remove("hide")
+    currentQuestionIndex ++
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const questions = [
+    {
+        question: "Pergunta1",
+        answers: [
+            {text: "resposta1a", correct: true},
+            {text: "resposta1b", correct: false},
+            {text: "resposta1c", correct: false},
+            {text: "resposta1d", correct: false},
+        ]
+    },
+    {
+        question: "Pergunta2",
+        answers: [
+            {text: "respostada2a", correct: true},
+            {text: "respostada2b", correct: false},
+            {text: "respostada2c", correct: false},
+            {text: "respostada2d", correct: false},
+        ]
+    },
+    {
+        question: "Pergunta3",
+        answers: [
+            {text: "respostada3a", correct: true},
+            {text: "respostada3b", correct: false},
+            {text: "respostada3c", correct: false},
+            {text: "respostada3d", correct: false},
+        ]
+    },
+]
