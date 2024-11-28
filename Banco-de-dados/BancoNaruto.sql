2ece8ebb2ba8fbe8fe28075ae2e479b0c2742c63
macari-11 <macari.pinho@sptech.school>
2024-11-27 21:24:13 -0300

    Atualização das telas de Personagens

--- Banco-de-dados/BancoNaruto.sql
+++ Banco-de-dados/BancoNaruto.sql
@@ -1,44 +1,26 @@
 create database Naruto;
-
 use naruto;
 
+
+drop table naruto;
 create table naruto (
 idCadastro int primary key auto_increment, 
 nome varchar(45),
 email varchar(45),
-senha varchar(45)
+senha varchar(45),
+preferido varchar(45)
 );
 
 select * from naruto;
-
 truncate table naruto;
+
+
+
 
 create table respostasQuiz (
 id int primary key auto_increment,
 resultado int 
 );
 
-
-insert into respostasQuiz (resultado) values
-(8),
-(9),
-(10),
-(7),
-(6),
-(8),
-(9),
-(6),
-(10);
-
-truncate table respostasQuiz;
-
-select * from respostasQuiz 
-order by resultado desc 
-limit 5;
-
-create table pontuacao (
-idPontuacao int primary key auto_increment,
-pontuacao int);
-
-
 select * from respostasQuiz;
+truncate table respostasQuiz;
