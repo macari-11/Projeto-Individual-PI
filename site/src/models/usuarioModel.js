54cb623cc92176c6e3bfb06e7e6c26394b14c74e
macari-11 <macari.pinho@sptech.school>
2024-11-27 23:53:09 -0300

    Mudança no nome da tabela do Cadastro

--- site/src/models/usuarioModel.js
+++ site/src/models/usuarioModel.js
@@ -3,7 +3,7 @@ var database = require("../database/config")
 function autenticar(email, senha) {
     console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function entrar(): ", email, senha)
     var instrucaoSql = `
-        SELECT nome, email, senha FROM naruto WHERE email = '${email}' AND senha = '${senha}';
+        SELECT nome, email, senha FROM cadastro WHERE email = '${email}' AND senha = '${senha}';
     `;
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
@@ -16,7 +16,7 @@ function cadastrar(nome, email, senha, preferido) {
     // Insira exatamente a query do banco aqui, lembrando da nomenclatura exata nos valores
     //  e na ordem de inserção dos dados.
     var instrucaoSql = `
-        INSERT INTO naruto (nome, email, senha, preferido) VALUES ('${nome}', '${email}', '${senha}', '${preferido}');
+        INSERT INTO cadastro (nome, email, senha, preferido) VALUES ('${nome}', '${email}', '${senha}', '${preferido}');
     `;
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
