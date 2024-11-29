c0fc5ee0d16967fd288c770358b70af4ed72dfa9
macari-11 <macari.pinho@sptech.school>
2024-11-29 20:03:18 -0300

    Atualização header

---
+++ site/public/site/header.js
@@ -0,0 +1,11 @@
+
+function AbrirOpcoes(){
+
+    const menuOptions = menu
+
+    if (menuOptions.style.display === "block") {
+        menuOptions.style.display = "none";
+    } else {
+        menuOptions.style.display = "block";
+    }
+}
